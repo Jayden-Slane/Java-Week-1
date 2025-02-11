def9e51913a8dfc6f2747b061232b78c2b74dd09
Jayden-Slane <jaydenslane123@gmail.com>
2025-02-11 10:05:31 -0500

    a

---
+++ Comments.java
@@ -0,0 +1,21 @@
+/***********************
+ * Project : Demo for Comments
+ * Programer : Jayden Slane
+ * Date : 2/11
+ * Program Name : Comments.java
+ ***********************/
+
+
+public class Comments {
+    
+    public static void main (String[]args)
+    {
+        System.out.println("Hi");
+        System.out.println("LabOneA");
+
+
+        
+    }
+
+
+}
